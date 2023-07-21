35b214633c26eec9913f4803f5873d3b29320653
GaspaObito <92884159+GaspaObito@users.noreply.github.com>
2023-07-21 17:19:53 -0500

    Add files via upload

--- Config/auth/conectar_RegistroUsuario.php
+++ Config/auth/conectar_RegistroUsuario.php
@@ -1,7 +1,7 @@
 <?php
 include("../Conexion.php");
 session_start();
-/* Inicio Sesion */
+/* Inicio Sesion PROFESOR,ADMIN*/
 if (isset($_POST["button_Auth"])) {
     $Correo = $_POST['Correo'];
     $Contrasena = $_POST['Contrasena'];
@@ -11,12 +11,12 @@ if (isset($_POST["button_Auth"])) {
     $resultado = $sentencia->get_result();
     if ($fila = $resultado->fetch_assoc()) {
         if ($fila['Email'] == 'Admin@Admin.com' && password_verify($Contrasena, $fila['Contrasena'])) {
-            $_SESSION['Id_usuario'] = $fila['Id_Profesor'];
+            $_SESSION['Id_Profe'] = $fila['Id_Profesor'];
             $_SESSION['Id_Admin'] = $fila['Id_Profesor'];
             echo "<script>alert('USUARIO ADMINISTRADOR CORRECTO')</script>";
             echo "<script>location.href='../../Admin/Profesor_busc_Admin.php'</script>";
         } elseif (password_verify($Contrasena, $fila['Contrasena'])) {
-            $_SESSION['Id_usuario'] = $fila['Id_Profesor'];
+            $_SESSION['Id_Profe'] = $fila['Id_Profesor'];
             echo "<script>alert('USUARIO PROFESOR CORRECTO')</script>";
             echo "<script>location.href='../../Profesor/anotaciones_busc.php'</script>";
         } else {
@@ -29,6 +29,27 @@ if (isset($_POST["button_Auth"])) {
     }
     $sentencia->close();
     $conexion->close();
+}
+/* Inicio Sesion ESTUDIANTE*/
+if (isset($_POST["LoginStudent"])) {
+    if (empty($_POST["Identificacionn"])) {
+        echo "<script>alert('NO SE PUEDE DEJAR VACIO CAMPO N.I')</script>";
+        echo "<script>location.href='../../Login_Users/acudiente.php'</script>";
+    } else {
+        $Documento = $_POST["Identificacionn"];
+        $query = "SELECT * FROM observador WHERE Numero_Documento = '$Documento'";
+        $resultado = mysqli_query($conexion, $query);
+        if (mysqli_num_rows($resultado) > 0) {
+            echo "<script>alert('USUARIO CORRECTO')</script>";
+            echo "<script>location.href='../../Login_Users/historial_anotaciones.php'</script>";
+            $_SESSION['Id_Estudiante'] = $_POST['Identificacionn'];
+        } else {
+            echo "<script>alert('ID NO ENCONTRADO')</script>";
+            echo "<script>location.href='../../Login_Users/acudiente.php'</script>";
+        }
+        $resultado->close();
+        $conexion->close();
+    }
 }
 if (isset($_POST["Cerrar_Login"])) {
     session_destroy();
